172f9b5a7d4bdd76628a9dbd039a41f56e1a67b1
LeliD <drach@g.jct.ac.il>
2022-10-29 20:55:46 +0300

    Initial code + Welcome functions

---
+++ Stage0/Program2921.cs
@@ -0,0 +1,20 @@
+﻿namespace Targil0
+{
+    partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            Welcome2921();
+            Welcome1100();
+            Console.ReadKey();
+        }
+
+        private static void Welcome2921()
+        {
+            Console.Write("Enter your name: ");
+            string s = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first consule application", s);
+        }
+        static partial void Welcome1100();
+    }
+}
