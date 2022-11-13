46457bdbf3e6d52bf06a62e95c4b4d73aeeba271
LeliD <drach@g.jct.ac.il>
2022-11-13 18:35:42 +0200

    Dal function

--- DalList/DalOrder.cs
+++ DalList/DalOrder.cs
@@ -5,34 +5,35 @@ namespace Dal;
 
 public class DalOrder
 {
-
     /// <summary>
     /// Adds an order to OrdersList
     /// </summary>
     /// <param name="order"></param>
-    /// <returns></returns>
+    /// <returns>returns Id of order</returns>
     public int Add(Order order)
     {
         order.ID = DataSource.Config.NextOrderNumber;
         DataSource.OrdersList.Add(order);
         return order.ID;
     }
+
     /// <summary>
-    /// 
+    /// Gets order by its Id
     /// </summary>
-    /// <param name="id"></param>
-    /// <returns></returns>
-    /// <exception cref="Exception"></exception>
+    /// <param name="id of order"></param>
+    /// <returns>returns the order</returns>
+    /// <exception cref="Exception">Throw exception if id of order doesn't exists</exception>
+
     public Order GetById(int id)
     {
         Order order = DataSource.OrdersList.Find(x => x?.ID == id)?? throw new Exception("Order doesn't exist");
         return order;
     }
     /// <summary>
-    /// 
+    /// Update order that exsist in the list
     /// </summary>
-    /// <param name="order"></param>
-    /// <exception cref="Exception"></exception>
+    /// <param name="orderis the object which being updated"></param>
+    /// <exception cref="Exception">Throw exception if order doesn't exist</exception>
     public void update(Order order)
     {
         if (!DataSource.OrdersList.Exists(x => x?.ID == order.ID))
@@ -43,19 +44,19 @@ public class DalOrder
         DataSource.OrdersList.Add(order);
     }
     /// <summary>
-    /// 
+    /// Gets all the orders in the list
     /// </summary>
-    /// <returns></returns>
+    /// <returns>return OrdersList</returns>
     public IEnumerable<Order?> GetAll()
     {
         IEnumerable<Order?>list=DataSource.OrdersList;
         return list;
     }
     /// <summary>
-    /// 
+    /// Deletes order from OrderItemsList
     /// </summary>
-    /// <param name="id"></param>
-    /// <exception cref="Exception"></exception>
+    /// <param name="id of order to delete"></param>
+    /// <exception cref="Exception">Throw exception if order doesn't exist</exception>
     public void delete(int id)
     {
         if (!DataSource.OrdersList.Exists(x => x?.ID == id))
--- DalList/DalProduct.cs
+++ DalList/DalProduct.cs
@@ -10,7 +10,7 @@ public class DalProduct
     /// Adds product to ProductsList
     /// </summary>
     /// <param name="product is the object which being added "></param>
-    /// <returns></returns>
+    /// <returns>returns Id of profuct</returns>
     /// <exception cref="Exception">Throw exception if id of product already exists</exception>
     public int Add(Product product)
     {
@@ -22,12 +22,24 @@ public class DalProduct
         DataSource.ProductsList.Add(product);
         return product.ID;
     }
+    /// <summary>
+    /// Gets product by its Id
+    /// </summary>
+    /// <param name="id of product"></param>
+    /// <returns>returns the product</returns>
+    /// <exception cref="Exception">Throw exception if id of product doesn't exists</exception>
     public Product GetById(int id)
     {
         Product product = DataSource.ProductsList.Find(x => x?.ID == id) ?? throw new Exception("Product doesn't exist");
 
         return product;
     }
+    /// <summary>
+    /// Update product that exsist in the list
+    /// </summary>
+    /// <param name="product is the object which being updated"></param>
+    /// <exception cref="Exception">Throw exception if product doesn't exist</exception>
+
     public void update(Product product)
     {
         if (!DataSource.ProductsList.Exists(x => x?.ID == product.ID))
@@ -38,12 +50,21 @@ public class DalProduct
        
         DataSource.ProductsList.Add(product);
     }
+    /// <summary>
+    /// Gets all the products in the list
+    /// </summary>
+    /// <returns>return ProductsList</returns>
     public IEnumerable<Product?> GetAll()
     {
         IEnumerable<Product?> list = DataSource.ProductsList;
         return list;
       
     }
+    /// <summary>
+    /// Deletes product from ProductsList
+    /// </summary>
+    /// <param name="id of product to delete"></param>
+    /// <exception cref="Exception">Throw exception if product doesn't exist</exception>
     public void delete(int id)
     {
         if (!DataSource.ProductsList.Exists(x => x?.ID == id))
