c44ff5ef3489c76862e949c61f144b4c6f3578c2
Shilat Sharon <shisharo@g.jct.ac.il>
2022-11-13 16:26:52 +0200

    product document

--- DalFacade/DO/Product.cs
+++ DalFacade/DO/Product.cs
@@ -6,11 +6,30 @@ namespace DO;
 /// </summary>
 public struct Product
 {
+    /// <summary>
+    /// Id of product
+    /// </summary>
     public int ID { get; set; }
+    /// <summary>
+    /// Name of product
+    /// </summary>
     public string? Name { get; set; }
+    /// <summary>
+    /// Price of product
+    /// </summary>
     public double Price { get; set; }
+    /// <summary>
+    /// Category of product
+    /// </summary>
     public Category Category { get; set; }
+    /// <summary>
+    /// The amount of product in stock
+    /// </summary>
     public int InStock { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns> returns String of Product representation</returns>
     public override string ToString() => $@"
 ID              =   {ID}
 Name            =   {Name} 
