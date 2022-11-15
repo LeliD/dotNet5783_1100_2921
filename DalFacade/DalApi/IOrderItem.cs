cd2a5f59cdb6a186f073cbd3b1e188b335222fc9
LeliD <drach@g.jct.ac.il>
2022-11-15 21:14:09 +0200

    Start stage2

---
+++ DalFacade/DalApi/IOrderItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DO;
+namespace DalApi
+{
+    public interface IOrderItem : ICrud<OrderItem>
+    {
+        OrderItem GetBy2Identifiers(int productID, int orderID);
+        IEnumerable<OrderItem?> GetItemsInOrder(int orderId);
+    }
+}
