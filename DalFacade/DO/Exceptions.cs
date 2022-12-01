713640850b9646fcdf3f89296309e7ca52e0a67a
Shilat Sharon <shisharo@g.jct.ac.il>
2022-12-01 22:03:57 +0200

    DO.Exceptions

--- DalFacade/DO/Exceptions.cs
+++ DalFacade/DO/Exceptions.cs
@@ -5,12 +5,31 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace DO;
-public class DalDoesNotExistException : Exception 
+[Serializable]
+public class DalMissingIdException : Exception 
 {
-    public DalDoesNotExistException(string? message) : base(message) { }
+    public int EntityID;
+    public string EntityName;
+    public DalMissingIdException(int id, string name)
+    : base() { EntityID = id; EntityName = name; }
+    public DalMissingIdException(int id, string name, string message)
+        : base(message) { EntityID = id; EntityName = name; }
+    public DalMissingIdException(int id, string name, string message, Exception innerException)
+        : base(message, innerException) { EntityID = id; EntityName = name; }
+    public override string ToString() =>
+        $"Id: {EntityID} of type {EntityName}, doesn't exist.";
 }
-
-public class DalAlreadyExistsException : Exception
+[Serializable]
+public class DalAlreadyExistIdException : Exception
 {
-    public DalAlreadyExistsException(string? message) : base(message) { }
+    public int EntityID;
+    public string EntityName;
+    public DalAlreadyExistIdException(int id, string name) 
+        : base() { EntityID = id; EntityName = name; }
+    public DalAlreadyExistIdException(int id, string name, string message)
+         : base(message) { EntityID = id; EntityName = name; }
+    public DalAlreadyExistIdException(int id, string name, string message, Exception innerException)
+        : base(message, innerException) { EntityID = id; EntityName = name; }
+    public override string ToString() => 
+        $"Id: {EntityID} of type {EntityName}, already exists.";
 }
