e5e1b02f7ffb303e961bda1a8e4fa1db9d03afb6
LeliD <drach@g.jct.ac.il>
2022-12-06 21:41:30 +0200

    add

--- PL/Product/ProductWindow.xaml.cs
+++ PL/Product/ProductWindow.xaml.cs
@@ -9,10 +9,12 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using BlApi;
 using BlImplementation;
+using BO;
 
 namespace PL.Product
 {
@@ -25,6 +27,24 @@ namespace PL.Product
         public ProductWindow()
         {
             InitializeComponent();
+            CategorySelector.ItemsSource = Enum.GetValues(typeof(BO.Category));
+        }
+
+        private void btnAddProduct_Click(object sender, RoutedEventArgs e)
+        {
+            int id;
+            bool check;
+            check = int.TryParse(tbId.Text, out id);
+            if (!check)
+                MessageBox.Show("The value must be int");
+            Category c;
+            check = Category.TryParse(CategorySelector.Text, out c);
+            if (!check)
+                MessageBox.Show("The value must be int");
+            bl.Product.AddProduct(new BO.Product() { ID= id,Category= (Convert)CategorySelector.Text, Name= tbName.Text,
+
+            });
+            
         }
     }
 }
