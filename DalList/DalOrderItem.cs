5c6874114a6e8eb14bab2f03b19ff40daa30781f
Shilat Sharon <shisharo@g.jct.ac.il>
2023-01-12 23:14:19 +0200

    12/01

--- DalList/DalOrderItem.cs
+++ DalList/DalOrderItem.cs
@@ -4,13 +4,12 @@ using DalApi;
 using DO;
 
 namespace Dal;
-
 internal class DalOrderItem : IOrderItem
 {
     /// <summary>
     ///  Adds order item to OrderItemsList
     /// </summary>
-    /// <param name="orderItem is the object which being added"></param>
+    /// <param name="orderItem"> is the object which being added</param>
     /// <returns>returns Id of order item</returns>
     public int Add(OrderItem orderItem)
     {
@@ -21,19 +20,19 @@ internal class DalOrderItem : IOrderItem
     /// <summary>
     /// Gets order item by its Id
     /// </summary>
-    /// <param name="id of order item"></param>
+    /// <param name="id">id of order item</param>
     /// <returns>returns the order item</returns>
-    /// <exception cref="Exception">Throw exception if id of order item doesn't exists</exception>
+    /// <exception cref="DalMissingIdException">Throws an exception if id of order item doesn't exists</exception>
     public OrderItem GetById(int id)
     {
         OrderItem orderItem = DataSource.OrderItemsList.Find(x => x?.ID == id)?? throw new DalMissingIdException(id, "OrderItem");
         return orderItem;
     }
     /// <summary>
-    /// Update order item that exsist in the list
+    /// Update order item that exists in the orders' list
     /// </summary>
-    /// <param name="orderItem is the object which being updated"></param>
-    /// <exception cref="Exception">Throw exception if order item doesn't exist</exception>
+    /// <param name="orderItem">orderItem is the object which being updated></param>
+    /// <exception cref="DalMissingIdException">Throws an exception if order item doesn't exist</exception>
     public void Update(OrderItem orderItem)
     {
         if (!DataSource.OrderItemsList.Exists(x => x?.ID == orderItem.ID))
@@ -41,10 +40,12 @@ internal class DalOrderItem : IOrderItem
         DataSource.OrderItemsList.RemoveAll(x => x?.ID == orderItem.ID);
         DataSource.OrderItemsList.Add(orderItem);
     }
+
     /// <summary>
-    /// Gets all the orderItems in the list in case no function was transfered. Otherwize, returns the filter list. 
+    ///  Gets all the orderItems in the list in case no function was transfered. Otherwize, returns the filter list. 
     /// </summary>
-    /// <returns>return IEnumerable<OrderItem?></returns>
+    /// <param name="filter">Func function</param>
+    /// <returns>IEnumerable<OrderItem?></returns>
     public IEnumerable<OrderItem?> GetAll(Func<OrderItem?, bool>? filter = null)
     {
         if (filter != null)
@@ -59,8 +60,9 @@ internal class DalOrderItem : IOrderItem
     /// <summary>
     /// Deletes order item from OrderItemsList
     /// </summary>
-    /// <param name="id of order item to delete"></param>
-    /// <exception cref="Exception">Throw exception if order item doesn't exist</exception>
+    /// <param name="id">id of order item to delete</param>
+    /// <exception cref="DalMissingIdException">Throws exception if order item doesn't exist</exception>
+
     public void Delete (int id)
     {
         if (!DataSource.OrderItemsList.Exists(x => x?.ID == id))
@@ -70,10 +72,10 @@ internal class DalOrderItem : IOrderItem
     /// <summary>
     /// Gets order item by its productID and orderID
     /// </summary>
-    /// <param name="productID of order item"></param>
-    /// <param name="orderID of order item"></param>
+    /// <param name="productID">productID of order item</param>
+    /// <param name="orderID">orderID of order item</param>
     /// <returns>returns order item</returns>
-    /// <exception cref="Exception">Throw exception if order item doesn't exist</exception>
+    /// <exception cref="DalMissingIdException">Throw exception if order item doesn't exist</exception>
     public OrderItem GetBy2Identifiers(int productID, int orderID)
     {
         OrderItem orderItem = DataSource.OrderItemsList.Find(x => x?.ProductID == productID && x?.OrderID == orderID)?? throw new DalMissingIdException(-1, "OrderItem");
@@ -82,8 +84,8 @@ internal class DalOrderItem : IOrderItem
     /// <summary>
     ///  Gets items in order
     /// </summary>
-    /// <param name="orderId of order items "></param>
-    /// <returns>return list of order items of the order</returns>
+    /// <param name="orderId">orderId of order items</param>
+    /// <returns>returns list of order items of the order</returns>
     public IEnumerable<OrderItem?> GetItemsInOrder(int orderId)
     {
         return DataSource.OrderItemsList.FindAll(x => x?.OrderID == orderId);
